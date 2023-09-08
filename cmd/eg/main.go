5da5dbb2864b51ad5a113ffbd4ca1a437cc50977
Peyman <107257794+ppeymann@users.noreply.github.com>
2023-09-08 08:56:46 +0300

    add main file

---
+++ cmd/eg/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	now := time.Now().UTC()
+
+	base := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.UTC).Unix()
+	start := time.Date(now.Year(), now.Month(), now.Day(), 7, 35, 0, 0, time.UTC).Unix()
+	end := time.Date(now.Year(), now.Month(), now.Day(), 23, 30, 0, 0, time.UTC).Unix()
+
+	fmt.Println("date:", base, "start:", start, "end:", end)
+}
